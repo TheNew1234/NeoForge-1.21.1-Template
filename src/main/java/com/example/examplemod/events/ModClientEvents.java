a5f64aa968b7b1a5e9b1885b9a4f7cbbb574205b
Yousef <buyouseeef@gmail.com>
2025-02-01 19:00:57 +0200

    Fixed crash because ModClientEvents doesn't have any SubscribeEvent annotated method, and also added folder entity to assets/template/textures

--- src/main/java/com/example/examplemod/events/ModClientEvents.java
+++ src/main/java/com/example/examplemod/events/ModClientEvents.java
@@ -2,9 +2,17 @@ package com.example.examplemod.events;
 
 import com.example.examplemod.TemplateMod;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientChatEvent;
+
 
 @EventBusSubscriber(modid = TemplateMod.MODID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
 public class ModClientEvents {
-
+    //The reason I created this empty method that has no use is because NeoForge crashes if any EventBusSubscriber doesn't
+    //any methods with the annotation SubscribeEvent.
+    @SubscribeEvent
+    public static void clientChat(ClientChatEvent event) {
+        //Do whatever you want
+    }
 }
